8b08d6511da30dc50b8153f195de288418c09480
r3dy <rajkumardulam2409@gmail.com>
2016-05-12 15:08:04 -0500

    local

--- src/javaLearing/Fibonacci.java
+++ src/javaLearing/Fibonacci.java
@@ -24,13 +24,11 @@ public class Fibonacci {
 				System.out.println("1st Line");
 				System.out.println("2nd Line");
 				System.out.println("3rd Line");
-<<<<<<< HEAD
+
 				System.out.println("4rd Line");
 				
 				System.out.println("How this possible");
-=======
-				System.out.println("4rd Line");
->>>>>>> branch 'master' of https://github.com/praveen1209/JavaPracticeGitHub.git
+			System.out.println("4rd Line");
 			}
 		}
 	}
